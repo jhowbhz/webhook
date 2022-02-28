b14915e51f8476aa8c4ea3d9a73e65555c0f3afc
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2022-02-27 21:57:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -36,6 +36,33 @@
                 fwrite($log, $data);
                 fclose($log);
 
+                $curl = curl_init();
+
+                curl_setopt_array($curl, array(
+                CURLOPT_URL => '%7B%7BSERVIDOR%7D%7D/sendText',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS =>'{
+                    "session": "{{session}}",
+                    "number" : "553195360492",
+                    "text" : "texto do caption para arquivo"
+                }',
+                CURLOPT_HTTPHEADER => array(
+                    'sessionkey: {{sessionkey}}',
+                    'Content-Type: application/json'
+                ),
+                ));
+
+                $response = curl_exec($curl);
+
+                curl_close($curl);
+                echo $response;
+
                 break;
             
             // por padrao retornamos error;
