7ddf339995aa780b5a53665dfb1d64a2c0a3c304
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2022-02-27 22:02:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
                 $curl = curl_init();
 
                 curl_setopt_array($curl, array(
-                CURLOPT_URL => '%7B%7BSERVIDOR%7D%7D/sendText',
+                CURLOPT_URL => 'https://whatsapp-v2.apibrasil.com.br/sendText',
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => '',
                 CURLOPT_MAXREDIRS => 10,
@@ -48,12 +48,12 @@
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => 'POST',
                 CURLOPT_POSTFIELDS =>'{
-                    "session": "{{session}}",
+                    "session": "teste22",
                     "number" : "553195360492",
                     "text" : "texto do caption para arquivo"
                 }',
                 CURLOPT_HTTPHEADER => array(
-                    'sessionkey: {{sessionkey}}',
+                    'sessionkey: teste22',
                     'Content-Type: application/json'
                 ),
                 ));
